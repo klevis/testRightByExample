8b4714be523af3472eb107c7cbd5f91be4ca3523
klevisgramo@gmail.com <Labelo4862?!BIBU>
2017-10-15 01:00:26 +0200

    added tests

---
+++ src/main/test/ramo/klevis/testing/change1/AddressConverterTest.java
@@ -0,0 +1,21 @@
+package ramo.klevis.testing.change1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by klevis.ramo on 10/15/2017.
+ */
+public class AddressConverterTest {
+    @Test
+    public void convertToModel() throws Exception {
+
+    }
+
+    @Test
+    public void convertToDbo() throws Exception {
+
+    }
+
+}
